c52441d63b5698a0e85469d531db6f327822a300
bollandj <joshuabolland21@gmail.com>
2021-09-26 16:40:38 +0100

    add TCC PWM driver with struct-based initialisation
    PWM driver using TCC - uses new struct-based initialisation format for multi-instance peripherals

---
+++ tcc_pwm.c
@@ -0,0 +1,49 @@
+/*
+ * tcc_pwm.c
+ *
+ * Created: 12/05/2021 17:48:05
+ *  Author: Joshua
+ */
+
+#include "tcc_pwm.h"
+#include "per_cfg.h"
+
+void tcc_pwm_init(Tcc *TCCX, tcc_pwm_hw_t *tcc_pwm_hw, tcc_pwm_cfg_t *tcc_pwm_cfg)
+{
+	/* Enable the APB clock for TCC */
+	PM->APBCMASK.reg |= tcc_pwm_hw->apbcmask;
+
+	/* Clock TCC0 with GCLK0 (for now) */
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(tcc_pwm_hw->clkctrl_id_val)
+					| GCLK_CLKCTRL_CLKEN
+					| GCLK_CLKCTRL_GEN(tcc_pwm_hw->gclk_val);
+	while (GCLK->STATUS.bit.SYNCBUSY);
+
+	/* Disable TCC */
+	while (TCCX->SYNCBUSY.bit.ENABLE);
+	TCCX->CTRLA.bit.ENABLE = 0;
+
+	/* Reset TCC */
+	while (TCCX->CTRLA.bit.SWRST);
+	TCCX->CTRLA.bit.SWRST = 1;
+	while (TCCX->SYNCBUSY.bit.SWRST || TCCX->CTRLA.bit.SWRST);
+
+	/* Set prescaler */
+	TCCX->CTRLA.bit.PRESCALER = tcc_pwm_cfg->prescaler_val;
+
+	/* Set period */
+	TCCX->PER.reg = tcc_pwm_cfg->period;
+
+	/* Use Normal PWM */
+	TCCX->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;
+	while (TCCX->SYNCBUSY.bit.WAVE);
+
+	/* Enable dead time generation */
+	//TCCX->WEXCTRL.reg = TCC_WEXCTRL_DTHS(8)
+	//					| TCC_WEXCTRL_DTLS(8)
+	//					| TCC_WEXCTRL_DTIEN0;
+
+	/* Enable output */
+	TCCX->CTRLA.bit.ENABLE = 1;
+	while (TCCX->SYNCBUSY.bit.ENABLE);
+}
---
+++ tcc_pwm.h
@@ -0,0 +1,60 @@
+/*
+ * tcc_pwm.h
+ *
+ * Created: 12/05/2021 17:48:14
+ *  Author: Joshua
+ */
+
+#ifndef TCC_PWM_H_
+#define TCC_PWM_H_
+
+#include "sam.h"
+#include "clock.h"
+#include "gpio.h"
+
+typedef struct
+{
+	uint8_t gclk_val;
+	uint32_t apbcmask;
+	uint32_t clkctrl_id_val;
+} tcc_pwm_hw_t;
+
+#define TCC_PWM_HW_TCC0 							\
+{													\
+	.gclk_val = 0,									\
+	.apbcmask = PM_APBCMASK_TCC0,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_TCC0_TCC1,	\
+}
+
+#define TCC_PWM_HW_TCC1 							\
+{													\
+	.gclk_val = 0,									\
+	.apbcmask = PM_APBCMASK_TCC1,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_TCC0_TCC1 	\
+}
+
+#define TCC_PWM_HW_TCC2								\
+{													\
+	.gclk_val = 0,									\
+	.apbcmask = PM_APBCMASK_TCC2,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_TCC2_TC3		\
+}
+
+typedef struct
+{
+	uint32_t period;
+	uint8_t prescaler_val;
+} tcc_pwm_cfg_t;
+
+#define TCC_PWM_CFG_DEFAULT 						\
+{ 													\
+	.period = 256,          						\
+	.prescaler_val = TCC_CTRLA_PRESCALER_DIV1_Val	\
+}
+
+void tcc_pwm_init(Tcc *TCCX, tcc_pwm_hw_t *tcc_pwm_hw, tcc_pwm_cfg_t *tcc_pwm_cfg);
+inline void tcc_pwm_set_duty(Tcc *TCCX, uint8_t channel, uint32_t duty) { TCCX->CCB[channel].reg = duty; }
+inline void tcc_pwm_set_period(Tcc *TCCX, uint32_t period) { TCCX->PERB.reg = period; }
+inline void tcc_pwm_set_prescaler(Tcc *TCCX, uint8_t prescaler_val) { TCCX->CTRLA.bit.PRESCALER = prescaler_val; }
+
+#endif /* TCC_PWM_H_ */
