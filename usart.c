5206ff49a27003bedc16a48a36f7653644850df7
bollandj <joshuabolland21@gmail.com>
2021-09-26 20:57:46 +0100

    modify USART driver for struct-based initialisation
    USART driver now uses hw, cfg and pinout structs for initialisation

--- usart.c
+++ usart.c
@@ -1,91 +1,101 @@
 
 #include "usart.h"
-#include "gpio.h"
 
-void usart_init()
+void usart_init(Sercom *SERCOMX, usart_hw_t *usart_hw, usart_cfg_t *usart_cfg, usart_pinout_t *usart_pinout)
 {
 	/* Enable APB clock for corresponding SERCOM with Power Manager */
-	PM->APBCMASK.reg |= USART_SERCOM_APBCMASK;
+	PM->APBCMASK.reg |= usart_hw->apbcmask;
 
-	/* Direct GCLK0 to SERCOM */
-	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(USART_SERCOM_CLKCTRL_ID_CORE_Val) |
-						GCLK_CLKCTRL_GEN(0) |
-						GCLK_CLKCTRL_CLKEN;
+	/* Clock SERCOM with selected GCLK */
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(usart_hw->clkctrl_id_val)
+					| GCLK_CLKCTRL_CLKEN
+					| GCLK_CLKCTRL_GEN(usart_hw->gclk_val);
 	while (GCLK->STATUS.bit.SYNCBUSY);
 
 	/* Wait for sync, then disable SERCOM */
-	while (USART_SERCOM->USART.SYNCBUSY.bit.ENABLE);
-	USART_SERCOM->USART.CTRLA.bit.ENABLE = 0;
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
+	SERCOMX->USART.CTRLA.bit.ENABLE = 0;
 
 	/* Reset the SERCOM */
-	while (USART_SERCOM->SPI.CTRLA.bit.SWRST);
-	USART_SERCOM->USART.CTRLA.bit.SWRST = 1;
-	while (USART_SERCOM->USART.CTRLA.bit.SWRST || USART_SERCOM->USART.SYNCBUSY.bit.SWRST);
-
-	/* Set up pin mux */
-	gpio_pin_set_pmux(USART_TX_PIN, USART_TX_PMUX_Val);
-	gpio_pin_set_pmux(USART_RX_PIN, USART_RX_PMUX_Val);
+	while (SERCOMX->SPI.CTRLA.bit.SWRST);
+	SERCOMX->USART.CTRLA.bit.SWRST = 1;
+	while (SERCOMX->USART.CTRLA.bit.SWRST || SERCOMX->USART.SYNCBUSY.bit.SWRST);
 
 	/* Configure MSB/LSB first, 16x oversampling + arithmetic baud rate, pinouts */
-	USART_SERCOM->USART.CTRLA.reg = (USART_LSB_FIRST << SERCOM_USART_CTRLA_DORD_Pos) |
-									SERCOM_USART_CTRLA_TXPO(USART_TXPO_Val) |
-									SERCOM_USART_CTRLA_RXPO(USART_RXPO_Val) |
-									SERCOM_USART_CTRLA_SAMPR(0) |
-									SERCOM_USART_CTRLA_RUNSTDBY |
-									SERCOM_USART_CTRLA_MODE_USART_INT_CLK;
+	SERCOMX->USART.CTRLA.reg = (usart_cfg->lsb_first << SERCOM_USART_CTRLA_DORD_Pos)
+							| SERCOM_USART_CTRLA_TXPO(usart_pinout->txpo_val)
+							| SERCOM_USART_CTRLA_RXPO(usart_pinout->rxpo_val)
+							| SERCOM_USART_CTRLA_SAMPR(0)
+							| SERCOM_USART_CTRLA_RUNSTDBY
+							| SERCOM_USART_CTRLA_MODE_USART_INT_CLK;
 
-	/* Set baud rate (arithmetic) */
-	USART_SERCOM->USART.BAUD.reg = 65536 - ((uint64_t)65536 * 16 * USART_BAUD) / SystemCoreClock;
+	/* Set baud rate (arithmetic)
+	 * TODO: this only works when the SERCOM is clocked from GCLK0 (core clock) */
+	SERCOMX->USART.BAUD.reg = 65536 - ((uint64_t)65536 * 16 * usart_cfg->baud) / SystemCoreClock;
 
 	/* Enable transmitter and receiver, 8 bit character size */
-	USART_SERCOM->USART.CTRLB.reg = SERCOM_USART_CTRLB_CHSIZE(0) |
-									SERCOM_USART_CTRLB_TXEN |
-									SERCOM_USART_CTRLB_RXEN;
-	while (USART_SERCOM->USART.SYNCBUSY.bit.CTRLB);
+	SERCOMX->USART.CTRLB.reg = SERCOM_USART_CTRLB_CHSIZE(0)
+							| SERCOM_USART_CTRLB_TXEN
+							| SERCOM_USART_CTRLB_RXEN;
+	while (SERCOMX->USART.SYNCBUSY.bit.CTRLB);
 
-	// Enable SERCOM */
-	USART_SERCOM->USART.CTRLA.bit.ENABLE = 1;
-	while (USART_SERCOM->USART.SYNCBUSY.bit.ENABLE);
+	/* Enable SERCOM */
+	SERCOMX->USART.CTRLA.bit.ENABLE = 1;
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
 }
 
-void usart_enable_interrupts(uint8_t interrupt_mask)
+void usart_enable_interrupts(Sercom *SERCOMX, IRQn_Type SERCOMX_IRQn, uint8_t interrupt_mask)
 {
 	/* Wait for sync, then disable SERCOM */
-	while (USART_SERCOM->USART.SYNCBUSY.bit.ENABLE);
-	USART_SERCOM->USART.CTRLA.bit.ENABLE = 0;
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
+	SERCOMX->USART.CTRLA.bit.ENABLE = 0;
 
 	/* Set interrupt mask */
-	USART_SERCOM->USART.INTENSET.reg = interrupt_mask;
+	SERCOMX->USART.INTENSET.reg = interrupt_mask;
 
 	/* Enable interrupt handler */
-	NVIC_SetPriority(USART_SERCOM_IRQn, 1 << __NVIC_PRIO_BITS);
-	NVIC_EnableIRQ(USART_SERCOM_IRQn);
+	NVIC_SetPriority(SERCOMX_IRQn, 1 << __NVIC_PRIO_BITS);
+	NVIC_EnableIRQ(SERCOMX_IRQn);
 
 	// Enable SERCOM */
-	USART_SERCOM->USART.CTRLA.bit.ENABLE = 1;
-	while (USART_SERCOM->USART.SYNCBUSY.bit.ENABLE);
+	SERCOMX->USART.CTRLA.bit.ENABLE = 1;
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
 };
 
-void usart_write(const uint8_t *data, uint32_t len)
+void usart_disable_interrupts(Sercom *SERCOMX, IRQn_Type SERCOMX_IRQn)
+{
+	/* Wait for sync, then disable SERCOM */
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
+	SERCOMX->USART.CTRLA.bit.ENABLE = 0;
+
+	/* Disable interrupt handler */
+	NVIC_DisableIRQ(SERCOMX_IRQn);
+
+	// Enable SERCOM */
+	SERCOMX->USART.CTRLA.bit.ENABLE = 1;
+	while (SERCOMX->USART.SYNCBUSY.bit.ENABLE);
+};
+
+void usart_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len)
 {
 	for (uint32_t i = 0; i < len; i++)
 	{
-		while (!USART_SERCOM->USART.INTFLAG.bit.DRE);
-		USART_SERCOM->USART.DATA.reg = data[i];
+		while (!SERCOMX->USART.INTFLAG.bit.DRE);
+		SERCOMX->USART.DATA.reg = data[i];
 	}
 }
 
-void usart_write_single(uint8_t data)
+void usart_write_single(Sercom *SERCOMX, uint8_t data)
 {
-	while (!USART_SERCOM->USART.INTFLAG.bit.DRE);
-	USART_SERCOM->USART.DATA.reg = data;
+	while (!SERCOMX->USART.INTFLAG.bit.DRE);
+	SERCOMX->USART.DATA.reg = data;
 }
 
-void usart_read(uint8_t *data, uint32_t len)
+void usart_read(Sercom *SERCOMX, uint8_t *data, uint32_t len)
 {
 	for (uint32_t i = 0; i < len; i++)
 	{
-		while (!USART_SERCOM->USART.INTFLAG.bit.RXC);
-		data[i] = USART_SERCOM->USART.DATA.reg;
+		while (!SERCOMX->USART.INTFLAG.bit.RXC);
+		data[i] = SERCOMX->USART.DATA.reg;
 	}
 }
--- usart.h
+++ usart.h
@@ -1,17 +1,63 @@
 
-
 #ifndef USART_H_
 #define USART_H_
 
 #include "sam.h"
-#include "per_cfg.h"
 
-void usart_init();
-void usart_enable_interrupts(uint8_t interrupt_mask);
+typedef struct
+{
+	uint8_t gclk_val;
+	uint32_t apbcmask;
+	uint32_t clkctrl_id_val;
+} usart_hw_t;
 
-void usart_write(const uint8_t *data, uint32_t len);
-void usart_write_single(uint8_t data);
-void usart_read(uint8_t *data, uint32_t len);
+#define USART_HW_SERCOM0 								\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM0,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM0_CORE_Val	\
+}
+
+typedef struct
+{
+	uint32_t baud;
+	uint8_t lsb_first;
+} usart_cfg_t;
+
+#define USART_CFG_DEFAULT	\
+{ 							\
+	.baud = 115200, 		\
+	.lsb_first = 1			\
+}
+
+#define USART_TXPO_PAD0 0
+#define USART_TXPO_PAD2 1
+
+#define USART_RXPO_PAD0 0
+#define USART_RXPO_PAD1 1
+#define USART_RXPO_PAD2 2
+#define USART_RXPO_PAD3 3
+
+typedef struct
+{
+	uint8_t txpo_val;
+	uint8_t rxpo_val;
+} usart_pinout_t;
+
+#define USART_PINOUT_DEFAULT		\
+{ 									\
+	.txpo_val = USART_TXPO_PAD0, 	\
+	.rxpo_val = USART_RXPO_PAD1		\
+}
+
+void usart_init(Sercom *SERCOMX, usart_hw_t *usart_hw, usart_cfg_t *usart_cfg, usart_pinout_t *usart_pinout);
+
+void usart_enable_interrupts(Sercom *SERCOMX, IRQn_Type SERCOMX_IRQn, uint8_t interrupt_mask);
+void usart_disable_interrupts(Sercom *SERCOMX, IRQn_Type SERCOMX_IRQn);
+
+void usart_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len);
+void usart_write_single(Sercom *SERCOMX, uint8_t data);
+void usart_read(Sercom *SERCOMX, uint8_t *data, uint32_t len);
 
 #endif /* USART_H_ */
 
