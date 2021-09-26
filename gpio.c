fa5ed4fdf0ed24231b948b638d7968113a18bae4
bollandj <joshuabolland21@gmail.com>
2021-09-26 17:30:14 +0100

    reorder function body for direction-setting functions

--- gpio.c
+++ gpio.c
@@ -9,11 +9,11 @@
 
 void gpio_port_set_input(uint32_t port_mask)
 {
+	PORT->Group[GROUP_PORTA].DIRCLR.reg = port_mask;
+
 	for (uint8_t i = 0; i < 32; i++)
 		if (port_mask & 1 << i)
 			PORT->Group[GROUP_PORTA].PINCFG[i].bit.INEN = 1;
-
-	PORT->Group[GROUP_PORTA].DIRCLR.reg = port_mask;
 }
 
 void gpio_port_set_pullup(uint32_t port_mask)
@@ -27,14 +27,14 @@ void gpio_port_set_pullup(uint32_t port_mask)
 
 void gpio_port_set_output(uint32_t port_mask)
 {
-	PORT->Group[GROUP_PORTA].DIRSET.reg = port_mask;
 	PORT->Group[GROUP_PORTA].OUTCLR.reg = port_mask;
+	PORT->Group[GROUP_PORTA].DIRSET.reg = port_mask;
 }
 
 void gpio_pin_set_input(uint8_t pin_no)
 {
-	PORT->Group[GROUP_PORTA].PINCFG[pin_no].bit.INEN = 1;
 	PORT->Group[GROUP_PORTA].DIRCLR.reg = 1 << pin_no;
+	PORT->Group[GROUP_PORTA].PINCFG[pin_no].bit.INEN = 1;
 }
 
 void gpio_pin_set_pullup(uint8_t pin_no)
@@ -45,20 +45,20 @@ void gpio_pin_set_pullup(uint8_t pin_no)
 
 void gpio_pin_set_output(uint8_t pin_no)
 {
-	PORT->Group[GROUP_PORTA].DIRSET.reg = 1 << pin_no;
 	PORT->Group[GROUP_PORTA].OUTCLR.reg = 1 << pin_no;
+	PORT->Group[GROUP_PORTA].DIRSET.reg = 1 << pin_no;
 }
 
-void gpio_pin_set_pmux(uint8_t pin_no, uint8_t pmux_val)
+void gpio_pin_set_pmux(uint8_t pin_no, uint8_t mux_val)
 {
 	/* Enable alternate pin function via pin mux */
 	PORT->Group[GROUP_PORTA].PINCFG[pin_no].bit.PMUXEN = 1;
 
 	/* Odd or even */
 	if (pin_no & 1)
-		PORT->Group[GROUP_PORTA].PMUX[pin_no >> 1].bit.PMUXO = pmux_val;
+		PORT->Group[GROUP_PORTA].PMUX[pin_no >> 1].bit.PMUXO = mux_val;
 	else
-		PORT->Group[GROUP_PORTA].PMUX[pin_no >> 1].bit.PMUXE = pmux_val;
+		PORT->Group[GROUP_PORTA].PMUX[pin_no >> 1].bit.PMUXE = mux_val;
 }
 
 void gpio_pin_clear_pmux(uint8_t pin_no)
--- gpio.h
+++ gpio.h
@@ -17,25 +17,19 @@ void gpio_port_set_input(uint32_t port_mask);
 void gpio_port_set_pullup(uint32_t port_mask);
 void gpio_port_set_output(uint32_t port_mask);
 
-inline void gpio_port_set(uint32_t port_mask)
-{ PORT->Group[GROUP_PORTA].OUTSET.reg = port_mask; }
-inline void gpio_port_clear(uint32_t port_mask)
-{ PORT->Group[GROUP_PORTA].OUTCLR.reg = port_mask; }
-inline void gpio_port_toggle(uint32_t port_mask)
-{ PORT->Group[GROUP_PORTA].OUTTGL.reg = port_mask; }
+inline void gpio_port_set(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTSET.reg = port_mask; }
+inline void gpio_port_clear(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTCLR.reg = port_mask; }
+inline void gpio_port_toggle(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTTGL.reg = port_mask; }
 
 void gpio_pin_set_input(uint8_t pin_no);
 void gpio_pin_set_pullup(uint8_t pin_no);
 void gpio_pin_set_output(uint8_t pin_no);
 
-inline void gpio_pin_set(uint8_t pin_no)
-{ PORT->Group[GROUP_PORTA].OUTSET.reg = 1 << pin_no; }
-inline void gpio_pin_clear(uint8_t pin_no)
-{ PORT->Group[GROUP_PORTA].OUTCLR.reg = 1 << pin_no; }
-inline void gpio_pin_toggle(uint8_t pin_no)
-{ PORT->Group[GROUP_PORTA].OUTTGL.reg = 1 << pin_no; }
+inline void gpio_pin_set(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTSET.reg = 1 << pin_no; }
+inline void gpio_pin_clear(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTCLR.reg = 1 << pin_no; }
+inline void gpio_pin_toggle(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTTGL.reg = 1 << pin_no; }
 
-void gpio_pin_set_pmux(uint8_t pin_no, uint8_t pmux_val);
+void gpio_pin_set_pmux(uint8_t pin_no, uint8_t mux_val);
 void gpio_pin_clear_pmux(uint8_t pin_no);
 
 
