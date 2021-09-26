a04585b5896629c61160ecfcd3c1eba871a49105
bollandj <joshuabolland21@gmail.com>
2021-09-26 21:58:16 +0100

    modify DAC driver for struct-based initialisation

--- dac.c
+++ dac.c
@@ -1,40 +1,39 @@
 
 #include "dac.h"
-#include "gpio.h"
-#include "per_cfg.h"
 
-void dac_init()
+void dac_init(Dac *DACX, dac_hw_t *dac_hw, dac_cfg_t *dac_cfg)
 {
 	/* Enable APB clock */
-	PM->APBCMASK.reg |= PM_APBCMASK_DAC;
+	PM->APBCMASK.reg |= dac_hw->apbcmask;
 
-	/* Direct CPU clock (GCLK0) to DAC */
-	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCLK_CLKCTRL_ID_DAC) |
-						GCLK_CLKCTRL_GEN(0) |
-						GCLK_CLKCTRL_CLKEN;
+	/* Direct selected GCLK to DAC */
+	/* NOTE: max. DAC GCLK frequency is 350kHz! */
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(dac_hw->clkctrl_id_val)
+					| GCLK_CLKCTRL_CLKEN
+					| GCLK_CLKCTRL_GEN(dac_hw->gclk_val);
+
 	while (GCLK->STATUS.bit.SYNCBUSY);
 
 	/* Wait for sync, then disable DAC */
-	while (DAC->STATUS.bit.SYNCBUSY);
-	DAC->CTRLA.bit.ENABLE = 0;
+	while (DACX->STATUS.bit.SYNCBUSY);
+	DACX->CTRLA.bit.ENABLE = 0;
 
 	/* Reset DAC */
-	DAC->CTRLA.bit.SWRST = 1;
-	while (DAC->STATUS.bit.SYNCBUSY);
+	DACX->CTRLA.bit.SWRST = 1;
+	while (DACX->STATUS.bit.SYNCBUSY);
 
-	/* Enable pin mux */
-	gpio_pin_set_pmux(DAC_VOUT_PIN, DAC_VOUT_PMUX_Val);
-
-	/* Enable external output, select AVCC as reference */
-	DAC->CTRLB.reg = DAC_CTRLB_EOEN | DAC_CTRLB_REFSEL(DAC_REFSEL_Val);
+	/* Enable external output */
+	DACX->CTRLB.reg = DAC_CTRLB_EOEN
+					| (dac_cfg->leftadj ? DAC_CTRLB_LEFTADJ : 0)
+					| DAC_CTRLB_REFSEL(dac_cfg->refsel);
 
 	/* Enable DAC */
-	DAC->CTRLA.bit.ENABLE = 1;
-	while (DAC->STATUS.bit.SYNCBUSY);
+	DACX->CTRLA.bit.ENABLE = 1;
+	while (DACX->STATUS.bit.SYNCBUSY);;
 }
 
-void dac_write(uint16_t val)
+void dac_write(Dac *DACX, uint16_t val)
 {
-	while (DAC->STATUS.bit.SYNCBUSY);
-	DAC->DATABUF.reg = val;
+	while (DACX->STATUS.bit.SYNCBUSY);
+	DACX->DATABUF.reg = val;
 }
--- dac.h
+++ dac.h
@@ -2,8 +2,35 @@
 #define DAC_H
 
 #include "sam.h"
+#include "clock.h"
 
-void dac_init();
-void dac_write(uint16_t val);
+typedef struct
+{
+	uint8_t gclk_val;
+	uint32_t apbcmask;
+	uint32_t clkctrl_id_val;
+} dac_hw_t;
+
+#define DAC_HW									\
+{												\
+	.gclk_val = CLOCK_GCLK1,					\
+	.apbcmask = PM_APBCMASK_DAC,				\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_DAC_Val	\
+}
+
+typedef struct
+{
+	uint8_t leftadj;
+	uint8_t refsel;
+} dac_cfg_t;
+
+#define DAC_CFG_DEFAULT					\
+{ 										\
+	.leftadj = 0, 						\
+	.refsel = DAC_CTRLB_REFSEL_AVCC_Val	\
+}
+
+void dac_init(Dac *DACX, dac_hw_t *dac_hw, dac_cfg_t *dac_cfg);
+void dac_write(Dac *DACX, uint16_t val);
 
 #endif // DAC_H
