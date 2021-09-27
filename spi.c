81e898054e3247af50066ecd71402ef9c5ffa8f8
bollandj <joshuabolland21@gmail.com>
2021-09-27 20:34:23 +0100

    implement struct-based initialisation for basic SPI driver, rename spim.h, spim.c to spi.h, spi.c

---
+++ spi.c
@@ -0,0 +1,71 @@
+/*
+ * spi.c
+ *
+ * Created: 11/05/2021 22:04:15
+ *  Author: Joshua
+ */
+
+#include "spi.h"
+#include "gpio.h"
+#include "per_cfg.h"
+
+void spi_init(Sercom *SERCOMX, spi_hw_t *spi_hw, spi_cfg_t *spi_cfg, spi_pinout_t *spi_pinout)
+{
+	/* Enable APB clock for corresponding SERCOM with Power Manager */
+	PM->APBCMASK.reg |= spi_hw->apbcmask;
+
+	/* Direct selected GCLK to SERCOM */
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(spi_hw->clkctrl_id_val)
+					| GCLK_CLKCTRL_CLKEN
+					| GCLK_CLKCTRL_GEN(spi_hw->gclk_val);
+	while (GCLK->STATUS.bit.SYNCBUSY);
+
+	/* Wait for sync, then disable SERCOM */
+	while (SERCOMX->SPI.SYNCBUSY.bit.ENABLE);
+	SERCOMX->SPI.CTRLA.bit.ENABLE = 0;
+
+	/* Wait for sync, then reset SERCOM */
+	while (SERCOMX->SPI.CTRLA.bit.SWRST);
+	SERCOMX->SPI.CTRLA.bit.SWRST = 1;
+	while (SERCOMX->SPI.SYNCBUSY.bit.SWRST || SERCOMX->SPI.SYNCBUSY.bit.ENABLE);
+
+	/* Configure as master */
+	SERCOMX->SPI.CTRLA.reg = SERCOM_SPI_CTRLA_MODE_SPI_MASTER
+							| SERCOM_SPI_CTRLA_DOPO(spi_pinout->dopo_val)
+							| SERCOM_SPI_CTRLA_DIPO(spi_pinout->dipo_val);
+
+	/* Set SPI mode (clock polarity + phase), MSB/LSB first */
+	SERCOMX->SPI.CTRLA.bit.CPOL = spi_cfg->polarity;
+	SERCOMX->SPI.CTRLA.bit.CPHA = spi_cfg->phase;
+	SERCOMX->SPI.CTRLA.bit.DORD = spi_cfg->lsb_first;
+
+	/* Set baud rate */
+	SERCOMX->SPI.BAUD.reg = SystemCoreClock / (2 * spi_cfg->baud) - 1;
+
+	/* Hardware Slave Select */
+	//SERCOMX->SPI.CTRLB.bit.MSSEN = 1;
+	//while (SERCOMX->SPI.SYNCBUSY.bit.CTRLB);
+
+	/* Enable SERCOM */
+	SERCOMX->SPI.CTRLA.bit.ENABLE = 1;
+	while (SERCOMX->SPI.SYNCBUSY.bit.ENABLE);
+}
+
+void spi_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len)
+{
+	for (uint8_t i = 0; i < len; i++)
+	{
+		while (!SERCOMX->SPI.INTFLAG.bit.DRE);
+		SERCOMX->SPI.DATA.reg = data[i];
+	}
+	/* Wait for final byte to be fully sent to facilitate software chip select */
+	while (!SERCOMX->SPI.INTFLAG.bit.TXC);
+}
+
+void spi_write_single(Sercom *SERCOMX, uint8_t data)
+{
+	while (!SERCOMX->SPI.INTFLAG.bit.DRE);
+	SERCOMX->SPI.DATA.reg = data;
+	/* Wait for byte to be fully sent to facilitate software chip select */
+	while (!SERCOMX->SPI.INTFLAG.bit.TXC);
+}
---
+++ spi.h
@@ -0,0 +1,105 @@
+/*
+ * spim.h
+ *
+ * Created: 11/05/2021 22:04:28
+ *  Author: Joshua
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "sam.h"
+
+typedef struct
+{
+	uint8_t gclk_val;
+	uint32_t apbcmask;
+	uint32_t clkctrl_id_val;
+} spi_hw_t;
+
+#define SPI_HW_SERCOM0 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM0,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM0_CORE_Val	\
+}
+
+#define SPI_HW_SERCOM1 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM1,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM1_CORE_Val	\
+}
+
+#define SPI_HW_SERCOM2 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM2,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM2_CORE_Val	\
+}
+
+#define SPI_HW_SERCOM3 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM3,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM3_CORE_Val	\
+}
+
+#define SPI_HW_SERCOM4 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM4,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM4_CORE_Val	\
+}
+
+#define SPI_HW_SERCOM5 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM5,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM5_CORE_Val	\
+}
+
+typedef struct
+{
+	uint32_t baud;
+	uint8_t polarity;
+	uint8_t phase;
+	uint8_t lsb_first;
+} spi_cfg_t;
+
+#define SPI_CFG_DEFAULT	\
+{ 						\
+	.baud = 115200,		\
+	.polarity = 0,		\
+	.phase = 0,			\
+	.lsb_first = 0		\
+}
+
+#define SPI_DOPO_DO_PAD0_SCK_PAD1 0
+#define SPI_DOPO_DO_PAD2_SCK_PAD3 1
+#define SPI_DOPO_DO_PAD3_SCK_PAD1 2
+#define SPI_DOPO_DO_PAD0_SCK_PAD3 3
+
+#define SPI_DIPO_DI_NONE 0
+#define SPI_DIPO_DI_PAD0 0
+#define SPI_DIPO_DI_PAD1 1
+#define SPI_DIPO_DI_PAD2 2
+#define SPI_DIPO_DI_PAD3 3
+
+typedef struct
+{
+	uint8_t dopo_val;
+	uint8_t dipo_val;
+} spi_pinout_t;
+
+#define SPI_PINOUT_DEFAULT					\
+{ 											\
+	.dopo_val = SPI_DOPO_DO_PAD0_SCK_PAD1, 	\
+	.dipo_val = SPI_DIPO_DI_PAD2			\
+}
+
+void spi_init(Sercom *SERCOMX, spi_hw_t *spi_hw, spi_cfg_t *spi_cfg, spi_pinout_t *spi_pinout);
+void spi_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len);
+void spi_write_single(Sercom *SERCOMX, uint8_t data);
+
+#endif /* SPI_H_ */
