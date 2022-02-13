a71b906ece860328d4749fbd39bfb0d129da8a58
bollandj <joshuabolland21@gmail.com>
2022-02-13 22:06:24 +0000

    Add GPIO read functions

--- gpio.h
+++ gpio.h
@@ -21,6 +21,8 @@ inline void gpio_port_set(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTSET.
 inline void gpio_port_clear(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTCLR.reg = port_mask; }
 inline void gpio_port_toggle(uint32_t port_mask) { PORT->Group[GROUP_PORTA].OUTTGL.reg = port_mask; }
 
+inline uint32_t gpio_port_read(uint32_t port_mask) { return PORT->Group[GROUP_PORTA].IN.reg & port_mask; }
+
 void gpio_pin_set_input(uint8_t pin_no);
 void gpio_pin_set_pullup(uint8_t pin_no);
 void gpio_pin_set_output(uint8_t pin_no);
@@ -28,6 +30,8 @@ void gpio_pin_set_output(uint8_t pin_no);
 inline void gpio_pin_set(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTSET.reg = 1 << pin_no; }
 inline void gpio_pin_clear(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTCLR.reg = 1 << pin_no; }
 inline void gpio_pin_toggle(uint8_t pin_no) { PORT->Group[GROUP_PORTA].OUTTGL.reg = 1 << pin_no; }
+
+inline uint8_t gpio_pin_read(uint8_t pin_no) { return (PORT->Group[GROUP_PORTA].IN.reg >> pin_no) & 1;}
 
 void gpio_pin_set_pmux(uint8_t pin_no, uint8_t mux_val);
 void gpio_pin_clear_pmux(uint8_t pin_no);
