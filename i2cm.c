ae92f7339c3504cca9318837fef5c8c9610acf45
bollandj <joshuabolland21@gmail.com>
2022-02-13 11:30:04 +0000

    Modify I2C driver for config structs
    I2C master driver now uses structs for configuration as with the
    existing SERCOM-based drivers

--- i2cm.c
+++ i2cm.c
@@ -2,126 +2,99 @@
 #include "i2cm.h"
 #include "gpio.h"
 
-void i2cm_init()
+void i2cm_init(Sercom *SERCOMX, i2cm_hw_t *i2cm_hw, i2cm_cfg_t *i2cm_cfg)
 {
 	/* Enable APB clock for corresponding SERCOM */
-	PM->APBCMASK.reg |= I2CM_SERCOM_APBCMASK;
+	PM->APBCMASK.reg |= i2cm_hw->apbcmask;
 
 	/* Direct GCLK0 (system clock) to SERCOM */
-	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(I2CM_SERCOM_CLKCTRL_ID_CORE_Val)
-						| GCLK_CLKCTRL_GEN(0)
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(i2cm_hw->clkctrl_id_val)
+						| GCLK_CLKCTRL_GEN(i2cm_hw->gclk_val)
 						| GCLK_CLKCTRL_CLKEN;
 	while (GCLK->STATUS.bit.SYNCBUSY);
 
 	/* Wait for sync, then disable SERCOM */
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.ENABLE);
-	I2CM_SERCOM->I2CM.CTRLA.bit.ENABLE = 0;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.ENABLE);
+	SERCOMX->I2CM.CTRLA.bit.ENABLE = 0;
 
 	/* Wait for sync, then reset SERCOM */
-	while (I2CM_SERCOM->I2CM.CTRLA.bit.SWRST);
-	I2CM_SERCOM->I2CM.CTRLA.bit.SWRST = 1;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SWRST || I2CM_SERCOM->I2CM.SYNCBUSY.bit.ENABLE);
+	while (SERCOMX->I2CM.CTRLA.bit.SWRST);
+	SERCOMX->I2CM.CTRLA.bit.SWRST = 1;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SWRST || SERCOMX->I2CM.SYNCBUSY.bit.ENABLE);
 
-	/* Set up pin mux */
-	gpio_pin_set_pmux(I2CM_SDA_PIN, I2CM_SDA_PMUX_Val);
-	gpio_pin_set_pmux(I2CM_SCL_PIN, I2CM_SCL_PMUX_Val);
-
-	/* Configure as master, 100/400kHz, SDA = PAD[0], SCK = PAD[1] */
-	I2CM_SERCOM->I2CM.CTRLA.reg = SERCOM_I2CM_CTRLA_SPEED(0)
-								  | SERCOM_I2CM_CTRLA_SDAHOLD(0)
+	/* Configure as master, SDA = PAD[0], SCK = PAD[1] */
+	SERCOMX->I2CM.CTRLA.reg = SERCOM_I2CM_CTRLA_SPEED(i2cm_cfg->speed)
+								  | SERCOM_I2CM_CTRLA_SDAHOLD(i2cm_cfg->sda_hold_time)
 								  | SERCOM_I2CM_CTRLA_MODE_I2C_MASTER;
 
 	/* Enable smart operation, quick command */
-	I2CM_SERCOM->I2CM.CTRLB.reg = SERCOM_I2CM_CTRLB_SMEN | SERCOM_I2CM_CTRLB_QCEN;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
+	SERCOMX->I2CM.CTRLB.reg = SERCOM_I2CM_CTRLB_SMEN | SERCOM_I2CM_CTRLB_QCEN;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 
 	/* Set baud rate */
-	I2CM_SERCOM->I2CM.BAUD.reg = SystemCoreClock / (2 * I2CM_BAUD) - 5;
+	SERCOMX->I2CM.BAUD.reg = SystemCoreClock / (2 * i2cm_cfg->baud) - 5;
 
 	/* Enable SERCOM */
-	I2CM_SERCOM->I2CM.CTRLA.bit.ENABLE = 1;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.ENABLE);
+	SERCOMX->I2CM.CTRLA.bit.ENABLE = 1;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.ENABLE);
 
 	/* Put bus into idle state */
-	I2CM_SERCOM->I2CM.STATUS.bit.BUSSTATE = I2CM_BUSSTATE_IDLE;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
+	SERCOMX->I2CM.STATUS.bit.BUSSTATE = I2CM_BUSSTATE_IDLE;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 }
 
-int8_t i2cm_start(uint8_t address)
+int8_t i2cm_start(Sercom *SERCOMX, uint8_t address)
 {
-	if (I2CM_SERCOM->I2CM.STATUS.bit.BUSSTATE == I2CM_BUSSTATE_BUSY)
+	if (SERCOMX->I2CM.STATUS.bit.BUSSTATE == I2CM_BUSSTATE_BUSY)
 		return -1;
 
 	/* Write address register */
-	I2CM_SERCOM->I2CM.ADDR.bit.ADDR = address << 1 | I2CM_WRITE;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
+	SERCOMX->I2CM.ADDR.bit.ADDR = address << 1 | I2CM_WRITE;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 
 	/* Wait for Master on Bus interrupt flag */
-	while (!I2CM_SERCOM->I2CM.INTFLAG.bit.MB);
+	while (!SERCOMX->I2CM.INTFLAG.bit.MB);
 
 	/* Don't continue transfer if not acknowledged */
-	if (I2CM_SERCOM->I2CM.STATUS.bit.RXNACK)
+	if (SERCOMX->I2CM.STATUS.bit.RXNACK)
 		return -1;
 
 	return 0;
 }
 
-int8_t i2cm_write(const uint8_t *data, uint32_t len)
+int8_t i2cm_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len)
 {
 	/* Write data */
 	for (uint8_t i = 0; i < len; i++)
 	{
-		I2CM_SERCOM->I2CM.DATA.reg = data[i];
-		while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
+		SERCOMX->I2CM.DATA.reg = data[i];
+		while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 
-		while (!I2CM_SERCOM->I2CM.INTFLAG.bit.MB);
+		while (!SERCOMX->I2CM.INTFLAG.bit.MB);
 
-		if (I2CM_SERCOM->I2CM.STATUS.bit.RXNACK)
+		if (SERCOMX->I2CM.STATUS.bit.RXNACK)
 			return -1;
 	}
 
 	return 0;
 }
 
-int8_t i2cm_write_single(uint8_t data)
+int8_t i2cm_write_single(Sercom *SERCOMX, uint8_t data)
 {
 	/* Write data */
-	I2CM_SERCOM->I2CM.DATA.reg = data;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
+	SERCOMX->I2CM.DATA.reg = data;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 
-	while (!I2CM_SERCOM->I2CM.INTFLAG.bit.MB);
+	while (!SERCOMX->I2CM.INTFLAG.bit.MB);
 
-	if (I2CM_SERCOM->I2CM.STATUS.bit.RXNACK)
+	if (SERCOMX->I2CM.STATUS.bit.RXNACK)
 		return -1;
 
 	return 0;
 }
 
-void i2cm_stop()
+void i2cm_stop(Sercom *SERCOMX)
 {
-	I2CM_SERCOM->I2CM.CTRLB.bit.CMD = I2CM_CMD_STOP_COND;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
-}
-
-int8_t i2cm_transfer(uint8_t address, const uint8_t *write_buf, uint8_t write_len, uint8_t *read_buf, uint8_t read_len)
-{
-	i2cm_start(address);
-
-	/* Write data */
-	for (uint8_t i = 0; i < write_len; i++)
-	{
-		I2CM_SERCOM->I2CM.DATA.reg = write_buf[i];
-		while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
-
-		while (!I2CM_SERCOM->I2CM.INTFLAG.bit.MB);
-
-		if (I2CM_SERCOM->I2CM.STATUS.bit.RXNACK)
-			return -1;
-	}
-
-	/* End by issuing stop condition */
-	I2CM_SERCOM->I2CM.CTRLB.bit.CMD = I2CM_CMD_STOP_COND;
-	while (I2CM_SERCOM->I2CM.SYNCBUSY.bit.SYSOP);
-
-	return 0;
+	SERCOMX->I2CM.CTRLB.bit.CMD = I2CM_CMD_STOP_COND;
+	while (SERCOMX->I2CM.SYNCBUSY.bit.SYSOP);
 }
--- i2cm.h
+++ i2cm.h
@@ -20,12 +20,84 @@
 #define I2CM_WRITE 0x00
 #define I2CM_READ  0x01
 
-void i2cm_init();
+typedef struct
+{
+	uint8_t gclk_val;
+	uint32_t apbcmask;
+	uint32_t clkctrl_id_val;
+} i2cm_hw_t;
 
-int8_t i2cm_start(uint8_t address);
-int8_t i2cm_write(const uint8_t *data, uint32_t len);
-int8_t i2cm_write_single(uint8_t data);
-void i2cm_stop();
+#define I2CM_HW_SERCOM0 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM0,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM0_CORE_Val	\
+}
+
+#define I2CM_HW_SERCOM1 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM1,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM1_CORE_Val	\
+}
+
+#define I2CM_HW_SERCOM2 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM2,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM2_CORE_Val	\
+}
+
+#define I2CM_HW_SERCOM3 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM3,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM3_CORE_Val	\
+}
+
+#define I2CM_HW_SERCOM4 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM4,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM4_CORE_Val	\
+}
+
+#define I2CM_HW_SERCOM5 									\
+{														\
+	.gclk_val = CLOCK_GCLK0,							\
+	.apbcmask = PM_APBCMASK_SERCOM5,					\
+	.clkctrl_id_val = GCLK_CLKCTRL_ID_SERCOM5_CORE_Val	\
+}
+
+#define I2CM_SPEED_STANDARD_FAST_MODE 0 /* Standard (<100kHz) or Fast (<400kHz) Mode */
+#define I2CM_SPEED_FAST_PLUS_MODE 1     /* Fast Mode Plus (<1MHz) */
+#define I2CM_SPEED_HIGH_SPEED_MODE 2    /* High-Speed Mode (<3.4MHz) */
+
+#define I2CM_SDAHOLD_DISABLED 0 
+#define I2CM_SDAHOLD_75NS 1
+#define I2CM_SDAHOLD_450NS 2
+#define I2CM_SDAHOLD_600NS 3
+
+typedef struct
+{
+	uint8_t speed;
+    uint32_t baud;
+	uint8_t sda_hold_time;
+} i2cm_cfg_t;
+
+#define I2CM_CFG_DEFAULT                    \
+{ 						                    \
+	.speed = I2CM_SPEED_STANDARD_FAST_MODE, \
+	.baud= 100000,		                    \
+	.sda_hold_time = I2CM_SDAHOLD_DISABLED  \
+}
+
+void i2cm_init(Sercom *SERCOMX, i2cm_hw_t *i2cm_hw, i2cm_cfg_t *i2cm_cfg);
+
+int8_t i2cm_start(Sercom *SERCOMX, uint8_t address);
+int8_t i2cm_write(Sercom *SERCOMX, const uint8_t *data, uint32_t len);
+int8_t i2cm_write_single(Sercom *SERCOMX, uint8_t data);
+void i2cm_stop(Sercom *SERCOMX);
 
 #endif /* I2CM_H_ */
 
